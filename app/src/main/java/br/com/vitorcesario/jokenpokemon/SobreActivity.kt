a21fa49a6d296a960ace9df76a642eac3e1a0fc5
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-28 09:33:46 -0300

    Initial commit

---
+++ app/src/main/java/br/com/vitorcesario/jokenpokemon/SobreActivity.kt
@@ -0,0 +1,12 @@
+package br.com.vitorcesario.jokenpokemon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SobreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sobre)
+    }
+}
