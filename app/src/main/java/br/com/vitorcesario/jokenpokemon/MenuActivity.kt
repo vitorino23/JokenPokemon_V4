c4ae840ae66ed42a3335e49be958bc2a5d0851ac
Vitor Cesario <vitorino.c23@gmail.com>
2018-10-05 09:48:56 -0300

    Fiz a splash

--- app/src/main/java/br/com/vitorcesario/jokenpokemon/MenuActivity.kt
+++ app/src/main/java/br/com/vitorcesario/jokenpokemon/MenuActivity.kt
@@ -1,12 +1,30 @@
 package br.com.vitorcesario.jokenpokemon
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        btJogar.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+        btSobre.setOnClickListener {
+            val intent = Intent(this, SobreActivity::class.java)
+            startActivity(intent)
+        }
+
+        btSair.setOnClickListener {
+            finish()
+        }
     }
+
+
 }
--- app/src/main/java/br/com/vitorcesario/jokenpokemon/SplashActivity.kt
+++ app/src/main/java/br/com/vitorcesario/jokenpokemon/SplashActivity.kt
@@ -1,12 +1,33 @@
 package br.com.vitorcesario.jokenpokemon
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
+
+    private val duracao = 3500L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        carregar()
+    }
+
+    fun carregar(){
+        val anim = AnimationUtils.loadAnimation(this,R.anim.animation)
+        anim.reset()
+        ivLogo!!.clearAnimation()
+        ivLogo!!.startAnimation(anim)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }, duracao)
     }
 }
